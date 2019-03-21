c37a5879f4526c063495576b3c65aef179df2570
Don Bowman <don@agilicus.com>
2019-03-21 12:13:22 -0400

    fix: set networkaddress.cache.ttl (default=60)
    The default config in the distroless java container
    is `networkaddress.cache.ttl=-1`. Set this (default 60).
    
    A new environment-variable (DRUID_NETWORKADDRESS_CACHE_TTL)
    may be used to override the default.

--- distribution/docker/druid.sh
+++ distribution/docker/druid.sh
@@ -133,5 +133,7 @@ then
     echo "$DRUID_LOG4J" > /tmp/conf/druid/_common/log4j2.xml
 fi
 
+echo networkaddress.cache.ttl=${DRUID_NETWORKADDRESS_CACHE_TTL:-60} > /tmp/conf/druid/_common/security.properties
+
 mkdir -p var/tmp var/druid/segments var/druid/indexing-logs var/druid/task var/druid/hadoop-tmp var/druid/segment-cache
-exec java ${JAVA_OPTS} -cp /tmp/conf/druid/_common:/tmp/conf/druid/${SERVICE}:lib/*: org.apache.druid.cli.Main server $@
+exec java ${JAVA_OPTS} -Djava.security.properties=/tmp/conf/druid/_common/security -cp /tmp/conf/druid/_common:/tmp/conf/druid/${SERVICE}:lib/*: org.apache.druid.cli.Main server $@
